08e20d2451c66142105982f20dd9848f317f5160
Grey Pegram <dpegram85@gmail.com>
2024-02-05 21:45:26 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if(array[i] + array[j] === target) {
+        return true
+      }
+    } 
+  } return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n^2)
+  The runtime grows quadratically based on the length of the input array since we have to loop 2 times
 */
 
 /* 
-  Add your pseudocode here
+  function that takes in an array and a target value
+
+  a for loop to establish 1st comparative value i
+    nested loop to establish second comparative value j
+      if statement to check if array[i] + array[j] equals target value
+      return true if yes
+      return false if no
 */
 
 /*
-  Add written explanation of your solution here
+   Function that intakes an array and a target sum integer
+  for loop through the array comparing sum value pairs
+  will need to loop through all possible pairs so nest another for loop to increment a second variable through each value PER each parent loop cycle
+  select array[i], compare to full loop of array[j], if no match
+  uptick i in array[i], compare to full loop of array[j]
+  j can equal i + 1 as we don't need to compare the value at the current [i] index to itself
+  return true when array[i] + array[j] equals the target value
+  or return false
 */
 
 // You can run `node index.js` to view these console logs
